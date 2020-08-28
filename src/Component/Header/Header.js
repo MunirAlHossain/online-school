d597f6c562dee9d4f10647c6c0bd196d8c5a8c03
MunirAlHossain <67576402+MunirAlHossain@users.noreply.github.com>
2020-08-28 23:14:28 +0600

    Update Header.js

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
 import './Header.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faUserGraduate } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <Container id="header">
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">
-//           <FontAwesomeIcon icon={faUserGraduate} />
+          <FontAwesomeIcon icon={faUserGraduate} />
             Online Courses</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
